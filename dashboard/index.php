a55d3ac1c3b12ebf8fcb6249603f03e747f68b7e
Bernard Mussche <bernardmussche@gmail.com>
2015-11-27 13:25:44 +0100

    Toevoegen HTML Dashboard
    CSS komt later

--- dashboard/index.php
+++ dashboard/index.php
@@ -17,10 +17,76 @@ if (isset($_SESSION['timeout']) && $_SESSION['timeout'] + SESSION_TIME < time())
 } else {
 	$_SESSION['timeout'] = time();
 }
-
-
-echo"ingelogd..!<br>";
-echo"<a href='/includes/logout.php'>Log uit</a>";
-
-
-
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Examen Analyse</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="theme-color" content="#1BBC9B">
+		<link rel="stylesheet" href="../assets/css/style.css" type="text/css" media="all">
+		<link rel="stylesheet" href="../assets/css/dashboard.css" type="text/css" media="all">
+	</head>
+	<body>
+		<div class="stickymenu">
+			<div class="titlemenu">
+				<div class="logoimg">
+					<img src="../images/dashboard/logo_fruytier.png" alt="logo_fruytier">
+					<img src="../images/dashboard/logo.png" alt="logo">
+				</div>
+				<div class="apptitle">
+					<h1>EXAMENANALYSE</h1>
+				</div>
+			</div>
+			<div class="usermenu">
+				<div class="headicon">
+					<img src="../images/dashboard/maleicon.png" alt="headicon">
+				</div>
+				<div class="username">
+					<h3>Bernard Mussche</h3>
+				</div>
+				<div class="settings">
+					<img src="../images/dashboard/settings.png" alt="settings">
+					<ul class="submenu">
+						<li>
+							<a href="#" class="submenuitem">Settings</a>
+						</li>
+						<li>
+							<a href="../includes/logout.php" class="submenuitem">Uitloggen</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="sidemenu">
+			<ul>
+				<a href="#" class="menulink">
+					<li class="menuheading">
+						Dashboard
+					</li>
+				</a>
+				<a href="#" class="menulink">
+					<li class="menuitem">
+						Examen
+					</li>
+				</a>
+				<a href="#" class="menulink">
+					<li class="menuitem">
+						Resultaten
+					</li>
+				</a>
+				<a href="#" class="menulink">
+					<li class="menuitem">
+						Score
+					</li>
+				</a>
+			</ul>
+		</div>
+		<div class="contentblock">
+			<div class="content">
+				<h1>INFORMATIE</h1>
+			</div>
+		</div>
+	</body>
+</html>
