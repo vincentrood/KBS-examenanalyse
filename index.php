1c5bea7e922653d629d17733fe1d3f38d8883919
sjaakvanlenten <sjaakvanlenten@hotmail.com>
2015-11-27 01:30:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php 
 require_once("includes/init.php");
 session_start();
-// hoi sjaak
-//test
+
 //Als gebruiker al is ingelogd , weer terugsturen naar het dashboard
 if (isset($_SESSION['gebruiker_id'])) {
 	header('Location: '  . BASE_URL . 'dashboard/');
