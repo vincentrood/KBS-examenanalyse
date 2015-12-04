0845c452b1b5c00225cadfad80415e8e9f9e9605
sjaakvanlenten <sjaakvanlenten@hotmail.com>
2015-12-04 16:03:39 +0100

    klassen/leerlingenlijst

---
+++ includes/partials/klassenlijst.html.php
@@ -0,0 +1,13 @@
+<?php 
+
+foreach($klassenlijst as $klas) {
+	echo "<tr>";
+	foreach($klas as $key => $value) {
+		echo '<td>' 
+		.($key == 'klas' ? '<a href="http://localhost/admin/leerlingenlijst.php?klas=' . $value . '">' : '') 
+		. $value  
+		. ($key == 'klas' ? '</a>' : '' )
+		. '</td>';
+	}
+	echo "</tr>";
+}
