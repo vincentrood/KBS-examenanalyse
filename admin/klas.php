dfd2af977f2d7386f9b24fd6241c8814a7888690
Bernard Mussche <bernardmussche@gmail.com>
2015-12-04 11:17:42 +0100

    Revert "Revert "css wijzigingen en header is toegevoegd aan alle bestanden""
    This reverts commit 0b98ba0eb4f143643b7e159213343a8137a7ba49.

--- admin/klas.php
+++ admin/klas.php
@@ -1,54 +1,7 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Examen Analyse</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<meta name="theme-color" content="#1BBC9B">
-		<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">-->
-		<link rel="stylesheet" href="../assets/css/style.css" type="text/css" media="all">
-		<link rel="stylesheet" href="../assets/css/dashboard.css" type="text/css" media="all">
-		<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
-	</head>
+	<?php include(ROOT_PATH . "includes/templates/header.php") ?>
 	<body>
-		<?php include(ROOT_PATH . "includes/partials/message.html.php"); ?>
-		<div class="stickymenu">
-			<div class="titlemenu">
-				<div class="logoimg">
-					<img src="../images/dashboard/logo_fruytier.png" alt="logo_fruytier">
-					<img src="../images/dashboard/logo.png" alt="logo">
-				</div>
-				<div class="apptitle">
-					<h1>EXAMENANALYSE</h1>
-				</div>
-			</div>
-			<div class="usermenu">
-				<div class="headicon">
-					<img src="../images/dashboard/maleicon.png" alt="headicon">
-				</div>
-				<div class="username">
-					<h3><?php if(isset($_SESSION['gebruiker_id'])) {
-										$data = getUserData($_SESSION['gebruiker_id']);echo $data['voornaam']." "
-							  					.$data['tussenvoegsel']." ".$data['achternaam'];?></h3>
-							  			<?php	} else {
-							  				echo "Gebruiker";
-							  			}?>
-
-
-				</div>
-				<div class="settings">
-					<img src="../images/dashboard/settings.png" alt="settings">
-					<ul class="submenu">
-						<li>
-							<a href="#" class="submenuitem">Settings</a>
-						</li>
-						<li>
-							<a href="../includes/logout.php" class="submenuitem">Uitloggen</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
 		<div class="sidemenu">
 			<ul>
 				<a href="/" class="menulink">
--- dashboard/index.php
+++ dashboard/index.php
@@ -28,46 +28,8 @@ if (isset($_SESSION['timeout']) && $_SESSION['timeout'] + SESSION_TIME < time())
 ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Examen Analyse</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<meta name="theme-color" content="#1BBC9B">
-		<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">-->
-		<link rel="stylesheet" href="../assets/css/style.css" type="text/css" media="all">
-		<link rel="stylesheet" href="../assets/css/dashboard.css" type="text/css" media="all">
-	</head>
+	<?php include(ROOT_PATH . "includes/templates/header.php");?>
 	<body>
-		<div class="stickymenu">
-			<div class="titlemenu">
-				<div class="logoimg">
-					<img src="../images/dashboard/logo_fruytier.png" alt="logo_fruytier">
-					<img src="../images/dashboard/logo.png" alt="logo">
-				</div>
-				<div class="apptitle">
-					<h1>EXAMENANALYSE</h1>
-				</div>
-			</div>
-			<div class="usermenu">
-				<div class="headicon">
-					<img src="../images/dashboard/maleicon.png" alt="headicon">
-				</div>
-				<div class="username">
-					<h3><?php $data = getUserData($_SESSION['gebruiker_id']);echo $data['voornaam']." ".$data['tussenvoegsel']." ".$data['achternaam'];?></h3>
-				</div>
-				<div class="settings">
-					<img src="../images/dashboard/settings.png" alt="settings">
-					<ul class="submenu">
-						<li>
-							<a href="#" class="submenuitem">Settings</a>
-						</li>
-						<li>
-							<a href="../includes/logout.php" class="submenuitem">Uitloggen</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
 		<div class="sidemenu">
 			<ul>
 				<a href="/" class="menulink">
