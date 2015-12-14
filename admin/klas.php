3237b3af092680025deef567fa42715d9880caf9
Henri <henrivandemunt@gmail.com>
2015-12-14 10:19:29 +0100

    Revert "Master"

--- admin/klas.php
+++ admin/klas.php
@@ -8,30 +8,51 @@ checkIfAdmin();
 
 ?>
 <html>
+	<?php include(ROOT_PATH . "includes/templates/header.php") ?>
 	<body>
-		<?php include(ROOT_PATH . "includes/templates/header.php") ?>
-		<?php include(ROOT_PATH . "includes/templates/sidebar-admin.php");?>
-		<div class="wrapper">
-			<?php include(ROOT_PATH . "includes/templates/sidebar-admin.php"); ?>
-			<div class="page-wrapper">
-				<div class="container-fluid">
-					<div class="contentblock">
-						<div class="content">
-							<table class="table table-condensed table-bordered">
-							    <thead>
-							      <tr>
-							        <th>Klas</th>
-							        <th>Examenjaar</th>
-							        <th>Docent</th>
-							      </tr>
-							    </thead>
-							    <tbody>					    	
-							    	<?php include(ROOT_PATH . "includes/partials/klassenlijst.html.php") ?>
-								</tbody>
-							</table>
-						</div>
-					</div>
-				</div>	
+		<div class="sidemenu">
+			<ul>
+				<a href="/" class="menulink">
+					<li class="menuheading">
+						Dashboard
+					</li>
+				</a>
+				<a href="#" class="menulink">
+					<li class="menuitem">
+						Leerling(en) toevoegen
+					</li>
+				</a>
+				<a href="<?php echo BASE_URL; ?>admin/docent.php" class="menulink">
+					<li class="menuitem">
+						Docent Toevoegen
+					</li>
+				</a>
+				<a href="#" class="menulink">
+					<li class="menuitem">
+						Klas toevoegen
+					</li>
+				</a>
+				<a href="#" class="menulink">
+					<li class="menuitem">
+						Examens toevoegen
+					</li>
+				</a>
+				<a href="#" class="menulink itembottom">
+					<li class="menuitem">
+						Settings
+					</li>
+				</a>
+				<a href="../includes/logout.php" class="menulink itembottom">
+					<li class="menuitem">
+						Uitloggen
+					</li>
+				</a>
+			</ul>
+		</div>
+		<div class="contentblock">
+			<div class="content">
+				<h2>Voeg Klas Toe</h2>
+				
 			</div>
 		</div>
 		<?php include(ROOT_PATH . "includes/templates/footer.php");?>
